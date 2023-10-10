0ba5cb18a2c47556a15ab2c17b24396640e530c6
Murilo <monteiromurilo743@gmail.com>
2023-10-10 14:15:14 -0300

    recebendo parâmetros na rota

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+
+    console.log(id);
+
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
 app.get('/', (requisicao, resposta) => {
-   // resposta.sendFile(`${caminhoBase}/index.html`)
-   resposta.send("Algo")
+   resposta.sendFile(`${caminhoBase}/index.html`)
 })
 
 app.listen(3000, () => {
